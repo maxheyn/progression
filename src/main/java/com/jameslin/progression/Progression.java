832d7c97fa4684ef212b3ffbb3842cd24fe34be1
qing lin <qingl114@hotmail.com>
2021-08-28 16:59:02 -0400

    set up

---
+++ src/main/java/com/jameslin/progression/Progression.java
@@ -0,0 +1,10 @@
+package com.jameslin.progression;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class Progression implements DedicatedServerModInitializer{
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
